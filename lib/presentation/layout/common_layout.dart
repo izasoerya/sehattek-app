92802249bd84c7d8a3e4e8d5b366d4558a62929a
izasoerya <ihza.surya@gmail.com>
2025-03-24 13:17:01 +0700

    bloc provider

--- lib/presentation/layout/common_layout.dart
+++ lib/presentation/layout/common_layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sehattek_app/presentation/blocs/auth/auth_bloc.dart';
 
 class CommonLayout extends StatelessWidget {
   final Widget child;
@@ -7,18 +9,21 @@ class CommonLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromRGBO(220, 234, 254, 1), // Start color
-              Color.fromRGBO(239, 246, 255, 1), // End color
-            ],
-            begin: Alignment.centerRight, // Gradient starts at the top
-            end: Alignment.centerLeft, // Gradient ends at the bottom
+      body: BlocProvider(
+        create: (context) => AuthenticationBloc(),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(220, 234, 254, 1), // Start color
+                Color.fromRGBO(239, 246, 255, 1), // End color
+              ],
+              begin: Alignment.centerRight, // Gradient starts at the top
+              end: Alignment.centerLeft, // Gradient ends at the bottom
+            ),
           ),
+          child: child,
         ),
-        child: child,
       ),
     );
   }
