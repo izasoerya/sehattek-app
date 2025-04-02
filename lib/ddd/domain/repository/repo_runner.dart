57299f027e54884676cee8484fd8056a39219665
izasoerya <ihza.surya@gmail.com>
2025-04-02 10:30:37 +0700

    update status product

--- lib/ddd/domain/repository/repo_runner.dart
+++ lib/ddd/domain/repository/repo_runner.dart
@@ -1,3 +1,4 @@
+import 'package:sehattek_app/core/utils/enumeration.dart';
 import 'package:sehattek_app/ddd/domain/entities/entities_service_runner.dart';
 
 abstract class RepoRunner {
@@ -5,5 +6,9 @@ abstract class RepoRunner {
   Future<EntitiesServiceRunner> readRunner(String runnerId);
   Future<List<EntitiesServiceRunner>> readListRunner(String providerId);
   Future<EntitiesServiceRunner> updateRunner(EntitiesServiceRunner runner);
+  Future<List<EntitiesServiceRunner>> updateStatusProduct(
+    StatusType statusType,
+    String providerId,
+  );
   Future<void> deleteRunner(EntitiesServiceRunner runner);
 }
--- lib/ddd/infrastructure/infrastructure_runner.dart
+++ lib/ddd/infrastructure/infrastructure_runner.dart
@@ -1,3 +1,4 @@
+import 'package:sehattek_app/core/utils/enumeration.dart';
 import 'package:sehattek_app/ddd/domain/entities/entities_service_runner.dart';
 import 'package:sehattek_app/ddd/domain/repository/repo_runner.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -36,7 +37,29 @@ class InfrastructureRunner implements RepoRunner {
   Future<EntitiesServiceRunner> updateRunner(
       EntitiesServiceRunner runner) async {
     // TODO: implement updateRunner
+
     throw UnimplementedError();
+  }
+
+  @override
+  Future<List<EntitiesServiceRunner>> updateStatusProduct(
+      StatusType statusType, String productId) async {
+    try {
+      final updatedData = await Supabase.instance.client
+          .from('service_runner')
+          .update({'uid_status_product': statusType.uid})
+          .eq('uid_service_product', productId)
+          .select()
+          .single();
+      final res = await Supabase.instance.client
+          .from('service_runner')
+          .select()
+          .eq('uid_provider', updatedData['uid_provider']);
+      return res.map((e) => EntitiesServiceRunner.fromJSON(e)).toList();
+    } on Exception catch (e) {
+      print('error updating runner: $e');
+      return [];
+    }
   }
 
   @override
