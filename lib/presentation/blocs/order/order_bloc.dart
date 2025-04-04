a68cfad0be6aef28436741af757d37eef3547c7e
izasoerya <ihza.surya@gmail.com>
2025-04-04 18:48:05 +0700

    create order bloc

--- lib/presentation/blocs/order/order_bloc.dart
+++ lib/presentation/blocs/order/order_bloc.dart
@@ -5,8 +5,20 @@ import 'package:sehattek_app/presentation/blocs/order/order_state.dart';
 
 class OrderBloc extends Bloc<OrderEvent, OrderState> {
   OrderBloc() : super(OrderStateInitial()) {
-    on<OrderEventFetchListWithStatus>(_onFetchListOrderWithStatus);
+    on<OrderEventCreate>(_onCreateOrder);
     on<OrderEventUpdateStatus>(_onUpdateStatusOrder);
+    on<OrderEventFetchListWithStatus>(_onFetchListOrderWithStatus);
+  }
+
+  void _onCreateOrder(OrderEventCreate event, Emitter<OrderState> emit) async {
+    emit(OrderStateLoading());
+    try {
+      await ServiceOrder().createOrder(event.order, event.providerId);
+      final res2 = await ServiceOrder().readListOrder(event.providerId);
+      emit(OrderStateSuccess(res2));
+    } catch (e) {
+      emit(OrderStateError('An unexpected error occurred'));
+    }
   }
 
   void _onFetchListOrderWithStatus(
--- lib/presentation/blocs/order/order_event.dart
+++ lib/presentation/blocs/order/order_event.dart
@@ -1,6 +1,14 @@
 import 'package:sehattek_app/core/utils/enumeration.dart';
+import 'package:sehattek_app/ddd/domain/entities/entities_service_product.dart';
 
 abstract class OrderEvent {}
+
+class OrderEventCreate extends OrderEvent {
+  final EntitiesServiceProduct order;
+  final String providerId;
+
+  OrderEventCreate(this.order, this.providerId);
+}
 
 class OrderEventFetch extends OrderEvent {
   final String orderId;
