0f4ac94b253e8ae1c53fc1d547a47db5b235ceb4
izasoerya <ihza.surya@gmail.com>
2025-04-04 18:47:36 +0700

    refactor

--- lib/ddd/domain/entities/entities_service_product.dart
+++ lib/ddd/domain/entities/entities_service_product.dart
@@ -1,21 +1,21 @@
 class EntitiesServiceProduct {
   final String uid;
-  final String uidProvider;
   final String name;
   final String description;
   final String price;
+  final String customerName;
   final DateTime orderDate;
-  final DateTime updateAt;
+  final DateTime updatedAt;
   final DateTime createdAt;
 
   EntitiesServiceProduct({
     required this.uid,
-    required this.uidProvider,
     required this.name,
     required this.description,
     required this.price,
+    required this.customerName,
     required this.orderDate,
-    required this.updateAt,
+    required this.updatedAt,
     required this.createdAt,
   });
 
@@ -23,35 +23,35 @@ class EntitiesServiceProduct {
     try {
       return EntitiesServiceProduct(
         uid: map['uid']?.toString() ?? 'Unknown UID',
-        uidProvider: map['uid_provider']?.toString() ?? 'Unknown Provider',
         name: map['name'] ?? 'Unknown Name',
         description: map['description'] ?? 'No Description',
         price: map['price']?.toString() ?? '0',
+        customerName: map['customer_name'] ?? 'Unknown Customer',
         orderDate: map['order_datetime'] != null
             ? DateTime.parse(map['order_datetime'])
-            : DateTime.now(), // Default to current date if null
-        updateAt: map['update_at'] != null
-            ? DateTime.parse(map['update_at'])
-            : DateTime.now(), // Default to current date if null
+            : DateTime.now(),
+        updatedAt: map['updated_at'] != null
+            ? DateTime.parse(map['updated_at'])
+            : DateTime.now(),
         createdAt: map['created_at'] != null
             ? DateTime.parse(map['created_at'])
-            : DateTime.now(), // Default to current date if null
+            : DateTime.now(),
       );
     } catch (e) {
       print('Error parsing JSON: $map, Error: $e');
-      rethrow; // Re-throw the error to handle it in the calling code
+      rethrow;
     }
   }
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toJSON() {
     return {
       'uid': uid,
-      'uid_provider': uidProvider,
       'name': name,
       'description': description,
       'price': price,
+      'customer_name': customerName,
       'order_datetime': orderDate.toIso8601String(),
-      'update_at': updateAt.toIso8601String(),
+      'updated_at': updatedAt.toIso8601String(),
       'created_at': createdAt.toIso8601String(),
     };
   }
