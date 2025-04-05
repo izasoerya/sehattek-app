122bab4cf0d9a85907618ad6aaa3e0a8d410358d
izasoerya <ihza.surya@gmail.com>
2025-04-06 05:44:05 +0700

    ui

--- lib/core/widgets/atom/custom_table_cell.dart
+++ lib/core/widgets/atom/custom_table_cell.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sehattek_app/core/utils/enumeration.dart';
+import 'package:sizer/sizer.dart';
 
 class CustomTableCell extends StatefulWidget {
+  final String? label;
   final Widget? child;
-  final String label;
 
-  const CustomTableCell({super.key, required this.label, this.child});
+  const CustomTableCell({super.key, this.label, this.child});
 
   @override
   State<CustomTableCell> createState() => _CustomTableCellState();
@@ -21,6 +22,19 @@ class _CustomTableCellState extends State<CustomTableCell> {
         padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.5),
         alignment: Alignment.centerLeft,
         decoration: BoxDecoration(),
-        child: widget.child);
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            widget.label != null
+                ? Text(
+                    widget.label!,
+                    style: TextStyle(
+                      fontSize: 11.5.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                : widget.child!,
+          ],
+        ));
   }
 }
